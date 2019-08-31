343b32c284475cd43bd21558dbc1d2432a9d81e3
sapede <matheussapede@gmail.com>
2019-08-31 15:23:07 -0400

    final

--- nerd-board-webAPI/src/java/controller/CategoriaController.java
+++ nerd-board-webAPI/src/java/controller/CategoriaController.java
@@ -7,10 +7,10 @@ package controller;
 
 import java.util.List;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import nerdBoard.dao.ICategoriaDAO;
 import nerdBoard.dao.postgresql.CategoriaDAOImpl;
